e480b67dd45f187e0369a58bad1741b799d4c3ed
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-10-26 16:33:02 +0300

    removed cache files

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");// instantiation
+        Admin curAdmin = new Admin(12345, "Stephan", "xa1@LW42%3");// instantiation
         User curUser = new User(4455, "John");
+
+        //test run commands
         curAdmin.performOperation(new Update()); // Admin Updates the data.
         curUser.performOperation(new View()); // User can view the data.
     }
